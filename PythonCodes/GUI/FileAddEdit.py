f4fbe6fc735e18ec4d2757a3a0ddb82ca5d2465a
traceytnguyen <traceynguyen3306@gmail.com>
2025-06-28 19:32:33 -0500

    wrong file, git inception

--- PythonCodes/GUI/FileAddEdit.py
+++ PythonCodes/GUI/FileAddEdit.py
@@ -4,7 +4,14 @@ import tkinter as tk
 def openfileGUI():
     fileswindow = tk.Toplevel(root)
     fileswindow.title("Files")
-    fileswindow.geometry("300x200")
+
+    clipbutton = tk.Button(fileswindow, text="Clip", command=lambda:openeditor)
+
+
+def openeditor(title):
+    editor_window = tk.Toplevel(root)
+    editor_window.title(title)
+
 
 root = tk.Tk()
 button = tk.Button(root, text="Add/Edit Files", command=openfileGUI)
