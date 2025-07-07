28c99086a39f01ff718bab8e95eef19267ea2fe7
kingcrafts1 <139026316+kingcrafts1@users.noreply.github.com>
2025-07-07 00:05:44 -0500

    Jarvis.py additions

--- PythonCodes/Jarvis/Jarvis.py
+++ PythonCodes/Jarvis/Jarvis.py
@@ -1,9 +1,56 @@
-import time
+
+
+
+# Function to read from speech to text file
 def readfile():
     file = open("PythonCodes\\Speech\\text.txt", "rt")
     return file.readlines()
 
-print(readfile())
+
+def stop():
+    # stop function placeholder
+    print("STOP!")
+
+
+# variable setup
+wordlist = []
+previous = ""
+
 
 while(1):
-    print(readfile())
+    # Prepares the words from the textfile into a useable format.
+    words = str(readfile())
+    words = words.replace("]", "")
+    words = words.replace("[", "")
+    words = words.replace("'", "")
+    words.split(" ")
+    #print(words)
+
+    #resets the wordlist if more than 10 words are stored
+    if len(wordlist) > 10:
+        wordlist = []
+
+    # this if statement runs if jarvis is in words, in the wordlist, and there is a new message
+    if ((words.find("jarvis") >= 0) or "jarvis" in wordlist) and words != previous:
+        
+        # adds words to the wordlist
+        wordlist = wordlist + (words.split(" "))
+        
+
+        # Maybe turn on a light on raspberry pi here?
+
+        # Testing code
+        if "quit" in wordlist:
+            print("quit") 
+            stop()
+            break
+
+
+        #for testing
+        print(wordlist)
+    
+
+
+
+    #updates previous message KEEP AT END OF WHILE LOOP!
+    previous = words
