b3b80a73945bf570d908322da480871fd5596d64
MasterBaconMan <jcbhill1@gmail.com>
2025-08-10 19:09:15 -0500

    run.bat and launch updated

--- launch.py
+++ launch.py
@@ -8,6 +8,7 @@ install_obs = subprocess.run(["pip","install","obs-websocket-py"])
 install_audio = subprocess.run(["pip","install","pyaudio"])
 install_pygame = subprocess.run(["pip", "install", "pygame"])
 install_pyautogui = subprocess.run(["pip", "install", "pyautogui"])
+install_SpeechRecognition = subprocess.run(["pip", "install", "SpeechRecognition"])
 
 p1 = subprocess.Popen(["python",  "PythonCodes/Speech/STT.py"])
 p2 = subprocess.Popen(["python", "PythonCodes/Jarvis/Jarvis.py"])
